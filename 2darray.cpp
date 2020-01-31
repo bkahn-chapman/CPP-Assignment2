38e16d402aad22d45c837bcc1372e7c26d3d84fe
kahnbenjamina <bkahn@chapman.edu>
2020-01-31 20:49:58 +0000

    Added comments.

--- 2darray.cpp
+++ 2darray.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-  int a[4][5];
-  int count = 0;
+  int a[4][5]; //initialize the array (4 rows, 5 columns)
+  int count = 0; //counts how many numbers have been entered
 
-  for(int i = 0; i < 4; ++i)
+  for(int i = 0; i < 4; ++i) //runs through the four rows
   {
-    for(int j = 0; j < 5; ++j)
+    for(int j = 0; j < 5; ++j) //runs the five numbers in each row
     {
-      count = count + 1;
+      count = count + 1; //ups the number count
       cout << "Enter int number " << count << " please." << endl;
-      cin >> a[i][j];
+      cin >> a[i][j]; //inputs the number into the array at the current indexes
     }
   }
 
-  for(int i = 0; i < 4; ++i)
+  for(int i = 0; i < 4; ++i) //iterates through the four rows
   {
-    for(int j = 0; j < 5; ++j)
+    for(int j = 0; j < 5; ++j) //iterates through each row
     {
-      cout << a[i][j] << " ";
+      cout << a[i][j] << " "; //prints the number at the curret index in the array
     }
     cout << endl;
   }
