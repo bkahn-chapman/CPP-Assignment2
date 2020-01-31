9505154c6bb6876b5b1c828e7cffaa73934c2302
kahnbenjamina <bkahn@chapman.edu>
2020-01-31 20:13:02 +0000

    Added the ability to check to see if a number is negative or not before entering it in the loop.

--- intarray.cpp
+++ intarray.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
-
 using namespace std;
 
 int main(int argc, char **argv)
 {
   int numIntegers = 10;
-  int numberArray[numIntegers];
+  unsigned int numberArray[numIntegers];
 
   for(int i = 0; i < numIntegers; ++i)
   {
+    int numberIn = 0;
     cout << "Please enter a nonnegative integer." << endl;
-    cin >> numberArray[i];
+    cin >> numberIn;
+    if(numberIn >= 0)
+    {
+      numberArray[i] = numberIn;
+    }
+    else
+    {
+      cout << "Please enter only nonnegative integers." << endl;
+      i = i - 1;
+    }
   }
 
   for(int j = 0; j < numIntegers; ++j)
   {
     cout << numberArray[j] << " ";
   }
+
+  cout << endl;
 
   return 0;
 }
