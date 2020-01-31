136f5bdfa60b9ed7b3f33e3d07cc7f617858bdac
kahnbenjamina <bkahn@chapman.edu>
2020-01-31 20:57:12 +0000

    Added comments.

--- zeroboth.cpp
+++ zeroboth.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void zeroBoth(int &value1, int &value2)
+void zeroBoth(int &value1, int &value2) //receives two call-by-reference variables
 {
-  value1 = 0;
-  value2 = 0;
+  value1 = 0; //sets value1 to zero
+  value2 = 0; //sets value2 to zero
 }
 
 int main(int argc, char **argv)
 {
-  int numOne = 7;
-  int numTwo = 3;
-  zeroBoth(numOne, numTwo);
-  cout << numOne << " " << numTwo << endl;
+  int numOne = 7; //test numOne
+  int numTwo = 3; //test numTwo
+  zeroBoth(numOne, numTwo); //calls the function
+  cout << numOne << " " << numTwo << endl; //outputs the results for testing purposes
 }
